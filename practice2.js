a11968e32d91ca24e5887931d5ff9ea13a6c0a6c
thelensbender <ijinleifeshadare@gmail.com>
2024-09-24 13:43:26 +0100

    final

--- practice2.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById("menu-btn").addEventListener("click", function() {
-  const navLinks = document.getElementById("nav-links");
-  navLinks.classList.toggle("show");
-});
